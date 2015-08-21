60ace246906ecf2131d0e69bd382c0d081c31a12
dlee0113 <dlee0113@gmail.com>
2015-08-21 08:25:58 -0400

    updated main

--- leet_code/src/two_sum_data_structure_design/TwoSum.java
+++ leet_code/src/two_sum_data_structure_design/TwoSum.java
@@ -35,9 +35,14 @@ public class TwoSum {
 	
 	public static void main(String[] args) {
 		TwoSum twoSum = new TwoSum();
+		
 		twoSum.add(1);
 		twoSum.add(3);
 		twoSum.add(5);
+		
 		twoSum.print();
+		
+		System.out.println(twoSum.find(4));
+		System.out.println(twoSum.find(7));
 	}
 }
