42cd84be58d84ccdfc44fbc09a90f522124fcd10
dlee0113 <dlee0113@gmail.com>
2015-08-21 08:36:08 -0400

    inital upload

---
+++ leet_code/src/valid_palindrome/Solution.java
@@ -0,0 +1,31 @@
+package valid_palindrome;
+
+public class Solution {
+	public static boolean isPalindrome(String s) {
+		int i = 0, j = s.length() - 1;
+		while (i < j) {
+			// skip non-alphanumeric characters
+			while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+				i++;
+			}
+			// skip non-alphanumeric characters
+			while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+				j--;
+			}
+			
+			if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+				return false;
+			}
+			
+			i++;
+			j--;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+		System.out.println(isPalindrome("race a car"));
+	}
+}
